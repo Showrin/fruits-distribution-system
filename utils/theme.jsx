88bd3d6be8eb9b29a1be9400ffce6aa75f191638
Showrin <showrinece15@gmail.com>
2023-11-27 01:20:13 +0600

    :package: package: Add material UI libraries

---
+++ utils/theme.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#6c3",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      primary: "#333",
+    },
+  },
+});
